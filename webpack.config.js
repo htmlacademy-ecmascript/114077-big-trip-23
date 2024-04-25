dbc333ee69d92fb3e479af56f73b1bfccab37cc8
Ivan Zagainov <fohunoff@gmail.com>
2024-04-25 13:24:50 +0300

    chore(1.23): настраивает сборку и запуск сервера для разработки

---
+++ webpack.config.js
@@ -0,0 +1,28 @@
+import { dirname, resolve } from 'path';
+import CopyPlugin from 'copy-webpack-plugin';
+import { fileURLToPath } from 'url';
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
+
+export default {
+  entry: './src/main.js',
+  output: {
+    filename: 'bundle.[contenthash].js',
+    path: resolve(__dirname, 'build'),
+    clean: true,
+  },
+  devtool: 'source-map',
+  plugins: [
+    new CopyPlugin({
+      patterns: [
+        {
+          from: 'public',
+          globOptions: {
+            ignore: ['**/index.html'],
+          },
+        },
+      ],
+    }),
+  ],
+};
